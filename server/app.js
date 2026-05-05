f3d9a9626ff6f52fd71113cc8b82cd9038a147b3
Aditya <ac7011043@gmail.com>
2026-05-05 23:53:45 +0530

    fixed cors

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://simple-text-base-application.vercel.app/",
+    origin: "https://simple-text-base-application.vercel.app",
     credentials: true
 }));
 
